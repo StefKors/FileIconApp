5ce0884ff99fe0e07386a8fda46a7a6223426d6d
Stef Kors <stef.kors@gmail.com>
2024-03-12 18:34:18 +0100

    Initial Commit

---
+++ FileIconApp/FileIconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileIconAppApp.swift
+//  FileIconApp
+//
+//  Created by Stef Kors on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FileIconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
